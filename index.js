630dd66b27a5372be576d93d2370d713d6f70826
= <bruno@paschoalinoto.com>
2016-12-18 17:59:04 -0200

    Tudo pronto, acho

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
+banco_raw = banco_raw.slice(0, -1);
+var anos = [];
+var salas = [];
+var banco = {};
 
+function nomesala(str) {
+    return str[0] + "º " + str[1];
+}
+
+function makeoption(value, text) {
+    return "<option value=\"" + value + "\">" + text + "</option>";
+}
+
+
+var select = document.getElementById("sala");
+function popularSelect() {
+    for (var index in banco_raw) {
+        var entry = banco_raw[index].split(":");
+        var nome = entry[0];
+        var sala = entry[1];
+        var ano = sala[0];
+        if (anos.indexOf(ano) < 0) {
+            anos.push(ano);
+            select.innerHTML += makeoption(ano, "Todo o " + ano + "º");
+        }
+        if (salas.indexOf(sala) < 0) {
+            salas.push(sala);
+            select.innerHTML += makeoption(sala, nomesala(sala));
+            banco[sala] = [];
+        }
+        banco[sala].push(nome);
+        banco[sala].sort();
+    }
+}
+
+var lista = document.getElementById("lista");
+function listar(value) {
+    lista.innerHTML = "";
+    if (value == "none") return false;
+    for (var key in banco) {
+        if (!banco.hasOwnProperty(key)) continue;
+        if (key.indexOf(value) < 0) continue;
+        if (value.length == 1) {
+            lista.innerHTML += "<h2>" + nomesala(key) + "</h2>";
+        }
+        lista.innerHTML += banco[key].join("<br>") + "<br><br>";
+    }
+}
--- index.php
+++ index.php
@@ -1,11 +1,61 @@
+<?php
+
+$msg = "";
+$f = "banco.txt";
+
+if (isset($_POST["login"]) and isset($_POST["senha"])) {
+	$login = escapeshellarg($_POST["login"]);
+	$senha = escapeshellarg($_POST["senha"]);
+	$cmd = escapeshellcmd("./navegante.py $login $senha");
+	$output_lines = explode("\n", shell_exec($cmd));
+	if (count($output_lines) < 2) {
+		$msg = $output_lines[0];
+	} else {
+		$nome = $output_lines[0];
+		$sala = $output_lines[1];
+		$nomesala = $sala[0] . "º " . $sala[1];
+		$banco = file_get_contents($f);
+		if (strpos($banco, $nome) !== false) {
+			$msg = "Você já está na lista, e sua sala é o $nomesala";
+		} else {
+			$banco .= "$nome:${sala}§";
+			file_put_contents($f, $banco);
+			$msg = "Você é do $nomesala, e acabamos de te colocar na lista!";
+		}
+	}
+}
+
+
+?>
+
 <html>
 	<head>
 		<title>Descura sua sala</title>
 		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="index.css">
 	</head>
-	<body>
+	<body onload="popularSelect()">
 		<h1>Descubra sua sala</h1>
 		<br>
-		
+		Achar sua sala e entrar na lista é fácil.<br>
+		Basta entrar com seu login e senha da Sala Virtual/Moodle.<br>
+		<br>
+		<form action="index.php" method="POST">
+			<input type="text" name="login" placeholder="Login"><br>
+			<input type="password" name="senha" placeholder="Senha"><br>
+			<input type="submit" value="Bora!"><br>
+		</form>
+		<div id="msg"><?php echo $msg; ?></div><br>
+		<br>
+		Agora, para ver a lista, escolha uma sala ou ano:
+		<select id="sala" oninput="listar(this.value)">
+			<option value="none">- - - - -</option>
+		</select><br>
+		<br>
+		<div id="lista"></div>
+		<script>
+			var banco_raw = "<?php echo file_get_contents("banco.txt"); ?>".split("§");
+		</script>
+		<script src="index.js"></script>
 	</body>
 </html>
